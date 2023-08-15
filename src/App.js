6761f4e42309af23b35e649d7a74ca21bc0aae46
Anurag-tiwari-tech <68097159+Anurag-tiwari-tech@users.noreply.github.com>
2023-08-15 14:45:57 +0530

    Project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>GSynergy React Web Challenge - A Two Page React Web App</h3>
+    </div>
+  );
+}
+
+export default App;
